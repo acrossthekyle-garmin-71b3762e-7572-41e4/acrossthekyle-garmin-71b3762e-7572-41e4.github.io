934712f53b561068acc51ce581edf4a801dda0e8
Kyle <kylegilbert87@gmail.com>
2022-04-08 12:00:25 -0500

    Udpated help verbiage

--- src/garmin/Help.jsx
+++ src/garmin/Help.jsx
@@ -4,9 +4,9 @@ const Help = () => {
 	return (
     <>
       <p className="lead text-start">
-        If you have any issues with entering, or receiving, an Unlock Code please
-        reach out to me via email at <a href="mailto:acrossthekyle@gmail.com">acrossthekyle@gmail.com</a>, or
-        through the "Contact Developer" feature in the ConnectIQ mobile app.
+        If you have any issues with entering, or receiving, an Unlock Code, or have any issues/questions with any of my applications,
+        then please reach out to me via email at <a href="mailto:acrossthekyle@gmail.com">acrossthekyle@gmail.com</a>,
+        or through the "Contact Developer" feature in the ConnectIQ mobile app.
       </p>
       <h2 className="pb-4 mt-4 text-start"># Instructions for Entering an Unlock Code</h2>
       <p className="lead text-start">
