3c1b0ad40cd2ea6569d91c54edafd2ecd3bc9924
Kyle <kylegilbert87@gmail.com>
2022-04-09 00:12:46 -0500

    Updated help, added link from home screen to garmin area of website

--- src/garmin/Help.jsx
+++ src/garmin/Help.jsx
@@ -10,11 +10,26 @@ const Help = () => {
       </p>
       <h2 className="pb-4 mt-4 text-start"># Instructions for Entering an Unlock Code</h2>
       <p className="lead text-start">
-        Once you get the Unlock Code, open the Garmin ConnectIQ Mobile app (make sure
-          the widget isn't open on the watch). Enter the Unlock Code in the Settings
-        screen for this widget, and press Save. Close the Garmin ConnectIQ Mobile app.
-        Open the Garmin Connect mobile app, wait for connection to watch then re-open
-        the widget on the watch in order to allow for activation.
+        <ul>
+          <li>
+            Once you get the Unlock Code, open the Garmin ConnectIQ Mobile app (make sure the widget isn't open on the watch).
+          </li>
+          <li>
+            Enter the Unlock Code in the Settings screen for this widget, and press Save.
+          </li>
+          <li>
+            Close the Garmin ConnectIQ Mobile app.
+          </li>
+          <li>
+            Open the Garmin Connect mobile app (<strong>important:</strong> do not use the watch WiFi or LTE, it will not work), wait for a connection to the watch and then re-open the widget on the watch in order to allow for activation.
+          </li>
+          <li>
+            Leave the widget open on the watch for several seconds.
+          </li>
+          <li>
+            You can then check the Unlock Status from the settings menu on the watch (open the menu on the main widget screen), or from the Settings screen in the Garmin ConnectIQ Mobile app.
+          </li>
+        </ul>
       </p>
     </>
   );
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header className="mb-auto" />
 
 		  <main className="px-3">
 	    	<h1 className="pb-4 mt-4">acrossthekyle</h1>
+        <div className="text-center">
+          <button
+            type="button"
+            className="btn btn-lg btn-success fw-bold mt-4"
+            onClick={() => navigate('/garmin')}
+          >
+            Go to Garmin Apps
+          </button>
+        </div>
       </main>
 
       <footer className="mt-auto text-white-50" />
