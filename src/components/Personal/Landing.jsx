8e400e937ae946079c7f13f93b00b719b9d7e9e0
Kyle <kylegilbert87@gmail.com>
2022-04-29 11:24:26 -0500

    Updated landing page wording

--- src/components/Personal/Landing.jsx
+++ src/components/Personal/Landing.jsx
@@ -13,7 +13,7 @@ export const Landing = () => {
       <h1 aria-hidden="true">@acrossthekyle</h1>
       <p className="mt-4 w-75">
         Hello! My name is Kyle. I'm an avid <a href="https://instagram.com/acrossthekyle" className="link-light text-decoration-none" target="_blank" rel="noreferrer"><span className="text-decoration-underline">hiker</span> <img src={ExternalLinkIcon} className="external-link-icon" alt="" /></a>,
-        and developer, who has walked the Camino de Santiago, Alta Via, and Annapurna Circuit, to name a few. When I'm not hiking or
+        and developer, who has thru-hiked the Camino de Santiago, Alta Via, and Annapurna Circuit, to name a few. When I'm not hiking or
         developing React applications I am probably creating <button type="button" className="btn btn-transparent text-light p-0 text-decoration-underline" role="link" onClick={() => navigate('garmin')}>Garmin</button> watch
         apps.
       </p>
